cc5e59b0b765ab30a3405782080f68937d443e90
Lucas Coutinho <lucaspp1@ucl.br>
2020-11-17 23:07:35 -0300

    teste

--- utils/Injector.cs
+++ utils/Injector.cs
@@ -13,24 +13,17 @@ namespace MPL.utils
   {
       // repositories
       public static IUserRepository IUserRepository;
-
       public static TransportRepositoryImpl TransportRepositoryImpl;
       public static EmpreenderRepositoryImpl EmpreenderRepositoryImpl;
 
       // Controllers
-      //public static LoginController LoginController;
       public static ClientController ClientController;
 
       public static void initialize(){
-        Console.WriteLine("antes initi");
         IUserRepository = new UserRepositoryImpl(DataBaseGod._DATABASE.UsuarioConsumidor);
-        Console.WriteLine("meio initi");
         TransportRepositoryImpl = new TransportRepositoryImpl(DataBaseGod._DATABASE.ContaTransportador);
-        Console.WriteLine("meio initi 2");
         EmpreenderRepositoryImpl = new EmpreenderRepositoryImpl(DataBaseGod._DATABASE.UsuarioEmpreender);
-        Console.WriteLine("meio initi 2");
         ClientController = new ClientController(IUserRepository,TransportRepositoryImpl ,EmpreenderRepositoryImpl);
-        Console.WriteLine("final initi");
         
       }
       
