ae8114d00b495ed3a907dc3a344f22c91cf39e44
Lucasbk2 <lucaslopes@ucl.br>
2020-11-23 03:02:12 -0300

    views empreendedor 2

--- View/Empreendedor/Vendas/VisualizarProdutoscreen.cs
+++ View/Empreendedor/Vendas/VisualizarProdutoscreen.cs
@@ -2,14 +2,22 @@ using System;
 
 using static MPL.utils.ViewUtils;
 using MPL.View.interfaces;
-
+using MPL.model;
+using System.Collections.Generic;
 
 namespace MPL.View.Empreendedor.Vendas
 {
   public class VisualizarProdutoscreen : IScreen
   {
       public  void Show(){
-        ShowScreen("Listar produtos");
+        UsuarioEmpreendedor usuario = (MainViewManager.CurrentUser as UsuarioEmpreendedor);
+        List<ItemEstoque> lista = usuario.Estoque;
+        for(int i=0;i<lista.Count;i++){
+          ShowScreen((i+1)+" | Nome: "+lista[i].Produto.Nome+" | Quantidade: "+lista[i].Quantidade);
+        }
+        string input = GetInput("Digite algo para sair");
+        if(input != "͔")
+          MainViewManager.ChangeScreen(new MenuEmpreendedorScreen());
       }
   }
 }
